725c20aa074934e99c14a70a3a8b7b35f155be5a
Gerrit Schrader <gerrit.schrader@gmail.com>
2020-11-04 08:24:39 +0100

    update (intermediate state)

--- parse/pytaf_parser.py
+++ parse/pytaf_parser.py
@@ -1,6 +1,6 @@
 from typing import Iterator, Union
 
-from lark import Lark, LexError
+from lark import Lark, UnexpectedInput
 from lark.lexer import Lexer, Token
 from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedMap
@@ -9,43 +9,34 @@ from ruamel.yaml.comments import CommentedMap
 class PytafLexer(Lexer):
     """tokenize pytaf script"""
 
-    keywords = (
-        "issue",
-        "markers",
-        "before",
-        "after",
-        "steps",
-        "skip",
-        "flaky",
-        "use",
-        "input",
-        "output",
-        "assign",
-        "env",
-        "file",
-        "scan",
-    )
+    indents = {
+        "issue": (0,),
+        "markers": (0, 2),
+        "before": (0, 2),
+        "after": (0, 2),
+        "steps": (0,),
+        "skip": (2,),
+        "skip_env": (2,),
+        "flaky": (2,),
+        "use": (2,),
+        "input": (2,),
+        "output": (2,),
+    }
 
     def __init__(self, lexer_conf):
         pass
 
-    def lex(self, doc: Union[CommentedMap, str], line=0, column=0, context=None, level=1) -> Iterator[Token]:
-        if isinstance(doc, CommentedMap):
-            if context in ("input", "output"):
+    def lex(self, doc: Union[CommentedMap, str], line=0, column=0, context=None, indent=0) -> Iterator[Token]:
+        for key, value in doc.items():
+            line, column = doc.lc.data[key][0], doc.lc.data[key][1]
+            token_type = key.upper() if key in self.indents.keys() else "VALUE"
+            yield Token(type_=token_type, value=key, line=line, column=column)
+            if key in ("input", "output") and indent in self.indents[key]:
                 yield Token(type_="OBJECT", value=doc, line=line, column=column)
+            elif isinstance(value, CommentedMap):
+                yield from self.lex(doc=value, line=line, column=column, indent=indent + 1)
             else:
-                for key, value in doc.items():
-                    line, column = doc.lc.data[key][0], doc.lc.data[key][1]
-                    token_type = key.upper() if key in self.keywords else "VALUE"
-                    yield Token(type_=token_type, value=key, line=line, column=column)
-                    if level == 3 and key in ("input", "output"):
-                        context = key
-                    if value:  # avoid 'None'
-                        yield from self.lex(doc=value, line=line, column=column, context=context, level=level + 1)
-        elif isinstance(doc, str):
-            yield Token(type_="VALUE", value=doc, line=line, column=column)
-        else:
-            raise LexError(f"line {line})", f"column {column}", f"invalid token '{doc}'")
+                yield Token(type_="VALUE", value=key, line=line, column=column)
 
 
 class PytafParser(Lark):
@@ -59,27 +50,28 @@ class PytafParser(Lark):
     before: BEFORE step+
     after: AFTER step+
     steps: STEPS step+
-    step: VALUE skip? markers? flaky? before? after? use? input? output?
+    step: VALUE skip_env? skip? markers? flaky? before? after? use? input? output?
+    skip_env: SKIP_ENV VALUE
     skip: SKIP VALUE*
     flaky: FLAKY VALUE
     use: USE VALUE
-    input: INPUT input_parameter+
-    input_parameter: OBJECT
-    output: OUTPUT output_parameter+
-    output_parameter: OBJECT
-
-    %declare VALUE OBJECT ISSUE MARKERS BEFORE AFTER STEPS SKIP FLAKY USE INPUT OUTPUT ASSIGN ENV FILE SCAN
+    input: INPUT parameter+
+    output: OUTPUT parameter+
+    parameter: OBJECT
+    
+    %declare VALUE OBJECT INVALID ISSUE MARKERS BEFORE AFTER STEPS SKIP_ENV SKIP FLAKY USE INPUT OUTPUT
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         super(PytafParser, self).__init__(
             grammar=self.grammar,
             parser="lalr",
-            propagate_positions=True,
             lexer=PytafLexer,
+            # propagate_positions=True,
+            **kwargs
         )
 
     def parse(self, text: str, **kwargs):
         yaml = YAML()
-        text = yaml.load(text)
+        text = yaml.load(stream=text)
         return super().parse(text=text, **kwargs)
--- tests/test_pytaf_parser.py
+++ tests/test_pytaf_parser.py
@@ -1,134 +1,48 @@
 import pytest
-from lark import Tree, Token, LexError
+from lark import UnexpectedInput
 
 from parse.pytaf_parser import PytafParser
 
 
 @pytest.fixture
 def parser():
-    return PytafParser()
+    return PytafParser(debug=True)
 
 
-# issue
-
-
-def test_issue(parser):
-    tree = parser.parse(r"issue: abc")
-    assert tree == Tree(
-        "start",
-        [Tree("issue", [Token("ISSUE", "issue"), Token("VALUE", "abc")])],
-    )
-
-
-def test_issue_without_id_raises_error(parser):
-    with pytest.raises(LexError):
-        parser.parse(r"issue:")
-
-
-def test_issue_with_array_arg_raises_error(parser):
-    with pytest.raises(LexError):
-        parser.parse(r"issue: [abc]")
-
-
-# markers
-
-
-def test_markers(parser):
-    tree = parser.parse(
-        r"""
-    issue: abc
-    markers: abc"""
-    )
-    assert tree == Tree(
-        "start",
-        [
-            Tree("issue", [Token("ISSUE", "issue"), Token("VALUE", "abc")]),
-            Tree("markers", [Token("MARKERS", "markers"), Token("VALUE", "abc")]),
-        ],
-    )
-
-
-# before
-
-
-# after
-
-
-# skip
-
-
-def test_skip(parser):
+def test_grammar(parser):
     tree = parser.parse(
         r"""
     issue: test case id
-    steps:
-        test step id:
-            skip:
-    """
-    )
-    print("\n" + tree.pretty())
-
-
-def test_skip_2(parser):
-    tree = parser.parse(
-        r"""
-    issue: test case id
+    before:
+        wait:
+            input:
+                duration: 5
     steps:
         test step id:
             skip: reason
-    """
-    )
-    print("\n" + tree.pretty())
-
-
-# flaky
-
-
-# input
-
-
-def test_input(parser):
-    tree = parser.parse(
-        r"""
-    issue: test case id
-    steps:
-        test step id:
+            flaky: 3
             use: func call
             input:
-                param1: value
-                param2:
-                    assign: value
-                param3:
+                param:
                     env: value
-                param4:
-                    file: path
-                param5:
-                    scan: query
-    """
-    )
-    print("\n" + tree.pretty())
-
-
-# output
-
-
-def test_output(parser):
-    tree = parser.parse(
-        r"""
-    issue: test case id
-    steps:
-        test step id:
-            use: func call
             output:
                 param1: value
-                param2:
-                    assign: value
-                param3:
-                    env: value
-                param4:
-                    file: path
-                param5:
-                    scan: query
     """
     )
     print("\n" + tree.pretty())
+
+
+def test_grammar_error(parser):
+    try:
+        tree = parser.parse(
+            r"""
+        issue: test case id
+        steps:
+            test step id:
+            use: none
+    """
+        )
+        print("\n" + tree.pretty())
+    except UnexpectedInput as error:
+        # msg = error.get_context("")
+        pass
